789985f22e5d963ccdbe5009e009e05c13411022
alexey <alexey1235@mail.ru>
2018-01-31 01:14:45 +0300

    Create models: general.Payments,general.PaymentsStocks; and modify general.admin for it.

--- general/admin.py
+++ general/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Banks,Stores,Stocks,Stores_Categories
+from .models import Banks,Payments,Stores,Stocks,PaymentsStocks,Stores_Categories
 # Register your models here.
 
 class BanksAdmin(admin.ModelAdmin):
@@ -7,7 +7,13 @@ class BanksAdmin(admin.ModelAdmin):
 	list_max_show_all=1000
 	list_per_page=1000
 	search_fields = ['name']
-	
+
+class PaymentsAdmin(admin.ModelAdmin):
+	list_display=('name','url','url_bonus','url_stock')
+	list_max_show_all=1000
+	list_per_page=1000
+	search_fields = ['name']
+
 class StoresAdmin(admin.ModelAdmin):
 	list_display=('name','categories','url')
 	search_fields = ['name']
@@ -17,11 +23,17 @@ class StocksAdmin(admin.ModelAdmin):
 	list_display=('BankName','StoreName','stock_value')
 	list_editable=('stock_value',)
 	
+class PaymentsStocksAdmin(admin.ModelAdmin):
+	list_display = ('PaymentName', 'StoreName', 'stock_value')
+	list_editable = ('stock_value',)
+
 class Stores_CategoriesAdmin(admin.ModelAdmin):
 	list_display=('name',)
 
 admin.site.register(Banks,BanksAdmin)
+admin.site.register(Payments,PaymentsAdmin)
 admin.site.register(Stores,StoresAdmin)
 admin.site.register(Stocks,StocksAdmin)
+admin.site.register(PaymentsStocks,PaymentsStocksAdmin)
 admin.site.register(Stores_Categories,Stores_CategoriesAdmin)
 
--- general/models.py
+++ general/models.py
@@ -94,6 +94,49 @@ class Banks(models.Model):
         verbose_name_plural = u'Банки'
         ordering = ['name']
 
+class Payments(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+    url = models.URLField(
+        max_length=300,
+        blank=True,
+        verbose_name='Сайт'
+    )
+    url_logo = models.URLField(
+        max_length=300,
+        blank=True,
+        editable=False,
+        verbose_name='Логотип'
+    )
+    url_bonus = models.URLField(
+        max_length=300,
+        blank=True,
+        verbose_name='Ссылка на акции, бонусы и спецпредложения'
+    )
+    url_stock = models.URLField(
+        max_length=300,
+        blank=True,
+        verbose_name='Ссылка на партнёрские программы'
+    )
+    partners = models.ManyToManyField(
+        Stores,
+        through='PaymentsStocks',
+        related_name='payments'
+    )
+
+    def __str__(self):
+        return self.name
+
+    def url_href(self):
+        return '<a href="' + self.url + '">Ссылка</a>'
+
+    class Meta:
+        verbose_name = u'Платёжная система'
+        verbose_name_plural = u'Платёжные системы'
+        ordering = ['name']
+
 
 class Stocks(models.Model):
     id_bank = models.ForeignKey(
@@ -119,5 +162,35 @@ class Stocks(models.Model):
         return self.id_store
 
     class Meta:
-        verbose_name = u'Скидка'
-        verbose_name_plural = u'Скидки'
+        verbose_name = u'Скидка партнёра'
+        verbose_name_plural = u'Скидки партнёров'
+        db_table= 'general_banks_stores_stocks'
+
+
+class PaymentsStocks(models.Model):
+    id_payment = models.ForeignKey(
+        Payments,
+        on_delete=models.CASCADE,
+        verbose_name='Название банка'
+    )
+    id_store = models.ForeignKey(
+        Stores,
+        on_delete=models.CASCADE,
+        verbose_name='Партнёр банка'
+    )
+    stock_value = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        verbose_name='Скидка до,%'
+    )
+
+    def PaymentName(self):
+        return self.id_payment
+
+    def StoreName(self):
+        return self.id_store
+
+    class Meta:
+        verbose_name = u'Скидка платёжной системы'
+        verbose_name_plural = u'Скидки платёжных систем'
+        db_table= 'general_payments_stores_stocks'
