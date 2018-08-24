815cf1e8044fb34979593325a1a610273e2b4a5f
CyberPunk2049 <avzherebtsov@mail.ru>
2018-08-24 17:14:48 +0300

    1,4,5,7 beta 1.2

--- general/views.py
+++ general/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django import forms
 from django.db.models import Count, Max
+from django.db.models import F
+from django.db.models.functions import Greatest
 
 from general.forms import BaseSearchForm,PartnersSearchForm
 from general.models import Banks,Stores
+from discounts.models import Banks_Stores,Payments_Stores
 
 class StoresList(ListView):
     '''Класс для отображения списка партнёров'''
@@ -27,7 +29,7 @@ class StoresList(ListView):
     def get_context_data(self, **kwargs):
         context = super(StoresList, self).get_context_data(**kwargs)
         context['form'] = self.form
-        print(self.form.cleaned_data)
+
         # Произведем партнёров по полям формы
         if self.form.cleaned_data['search_field']!='':
             self.queryset = self.queryset.filter(name__icontains=self.form.cleaned_data['search_field'])
@@ -35,6 +37,16 @@ class StoresList(ListView):
             self.queryset = self.queryset.filter(name__istartswith=self.form.cleaned_data['choice_letter'])
         if self.form.cleaned_data['choice_category']!='':
             self.queryset = self.queryset.filter(categories__id=self.form.cleaned_data['choice_category'])
+
+        # Отфильтруем партнёров, не учавствующих в партнёрских программах на данный момент
+        disc_partners_list=Banks_Stores.objects.all().union(Payments_Stores.objects.all())
+        disc_partners_list=disc_partners_list.distinct('id_store').values_list('id_store',flat=True)
+        self.queryset=self.queryset.filter(id__in=disc_partners_list)
+
+        # Вычислим максимальную скидку банка и платёжной системы
+        self.queryset=self.queryset.annotate(max_discount_bank=Max('banks_stores__stock_value'))
+        self.queryset=self.queryset.annotate(max_discount_payment=Max('payments_stores__stock_value'))
+        self.queryset=self.queryset.annotate(max_discount=Greatest('max_discount_bank','max_discount_payment'))
 
         context['object_list']=self.queryset.values()
         context['view_name']=self.view_name
@@ -62,6 +74,8 @@ class BanksList(ListView):
         self.queryset=self.queryset.filter(name__icontains=self.form.cleaned_data['search_field'])
         #Произведем подсчёт партнёров банков
         self.queryset=self.queryset.annotate(count_partners=Count('partners')).filter(count_partners__gt=0)
+        #Произведем подсчёт максимальной скидки банка
+        self.queryset=self.queryset.annotate(max_discount=Max('banks_stores__stock_value'))
 
         context['object_list']=self.queryset.values()
         context['view_name'] = self.view_name
